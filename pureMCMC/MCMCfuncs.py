71807208358132529b0718f6dd226011773c8de1
Ismael Parada <ismaelp@kth.se>
2025-02-09 02:03:02 +0100

    new structure + speedup

--- pureMCMC/MCMCfuncs.py
+++ pureMCMC/MCMCfuncs.py
@@ -1,15 +1,92 @@
 import random
 import numpy as np
 import networkx as nx
-
+import ges
+import copy
+from generateDAGs import generate_colored_DAG
 
 # Main MCMC function
-def MCMC_iteration(edge_array, partition, samples, sorted_edges, possible_moves=None):
+
+def MCMC(samples, num_iters, move_list = None, start_from_GES = False):
+    num_nodes = samples.shape[1]
+    
+
+    if start_from_GES:
+        GES_edge_array = ges.fit_bic(data=samples)[0]
+
+        # Take an initial DAG from the given GES CPDAG
+        A =  GES_edge_array.copy()
+        double = []
+        for i in range(num_nodes):
+            for j in range(i+1, num_nodes):
+                if A[i,j] == 1 and A[j,i] == 1:
+                    double.append((i,j))
+                    A[i,j] = 0
+                    A[j,i] = 0
+
+        for edge in double:
+            new_edges = A.copy()
+            new_edges[edge[0], edge[1]] = 1
+            if is_DAG(new_edges):
+                A = new_edges
+                continue
+
+            new_edges = A.copy()
+            new_edges[edge[1], edge[0]] = 1
+            if is_DAG(new_edges):
+                A = new_edges
+                continue
+
+            raise ValueError("Could not create DAG")
+
+        # Every node has its own color
+        partition = [[i] for i in range(num_nodes)]
+
+    else:
+        # Fully random colored DAG
+        partition, A, _ = generate_colored_DAG(num_nodes, num_nodes, 0.5)
+        A = np.array(A != 0, dtype="int")
+    
+
+
+
+    # Setup for iters
+    sorted_edges = get_sorted_edges(A)
+    bic = score_DAG(samples, A, partition)
+
+
+    # Space for best found DAG
+    best_A = A.copy()
+    best_partition = partition.copy()
+    best_bic = bic
+    best_iter = 0
+
+
+    # Run MCMC iters    
+    for i in range(num_iters):
+
+        A, partition, bic, sorted_edges = MCMC_iteration(samples, A, partition, bic, sorted_edges, move_list)
+
+        if bic > best_bic:
+            best_A = A.copy()
+            best_partition = partition.copy()
+            best_bic = bic
+            best_iter = i
+
+    return best_A, best_partition, best_bic, best_iter
+
+
+
+
+
+def MCMC_iteration(samples, edge_array, partition, bic, sorted_edges, possible_moves=None):
     old_edge_array = edge_array.copy()
-    old_partition = partition.copy()
+    old_partition = copy.deepcopy(partition)
+    old_bic = bic
+
 
     potential_edge_array = edge_array.copy()
-    potential_partition = partition.copy()
+    potential_partition = copy.deepcopy(partition)
 
     
     edges_in_DAG, edges_giving_DAGs, edges_not_giving_DAGs = sorted_edges
@@ -76,9 +153,8 @@ def MCMC_iteration(edge_array, partition, samples, sorted_edges, possible_moves=
     # Metropolis hastings algorithm
 
     potential_bic = score_DAG(samples, potential_edge_array, potential_partition)
-    old_bic = score_DAG(samples, old_edge_array, old_partition)
-
-    if random.random() <= (potential_bic / old_bic) * q_quotient: 
+    
+    if random.random() <= (potential_bic / old_bic) * q_quotient:
         new_edge_array = potential_edge_array
         new_partition = potential_partition
         new_bic = potential_bic
@@ -94,7 +170,6 @@ def MCMC_iteration(edge_array, partition, samples, sorted_edges, possible_moves=
         new_partition = old_partition
         new_bic = old_bic
         new_sorted_edges = sorted_edges
-
 
     return new_edge_array, new_partition, new_bic, new_sorted_edges
 
@@ -176,7 +251,8 @@ def is_DAG(A):
 
 
 
-#For DAG heuristic
+# For DAG heuristic
+# Speedup by NOT doing edges of only color was changed and vice versa
 
 def score_DAG(samples, edge_array, partition):
     samples = np.transpose(samples)
@@ -216,7 +292,6 @@ def score_DAG(samples, edge_array, partition):
 
     return bic
 
-
 def get_parents(node, edge_array):
     parents = []
     n = np.shape(edge_array)[0]
@@ -224,6 +299,14 @@ def get_parents(node, edge_array):
         if edge_array[i, node] == 1:
             parents.append(i)
     return parents
+
+
+def generate_partition(no_nodes, no_colors):
+    partition = [[] for _ in range(no_colors)]
+    for node in range(no_nodes):
+        color = random.randrange(no_colors)
+        partition[color].append(node)
+    return partition
 
 def main():
     pass

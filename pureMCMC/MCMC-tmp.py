f0075baf591c8e064e19dfcf313ed3d0c35ceeaa
Ismael Parada <ismaelp@kth.se>
2025-02-09 02:03:19 +0100

    cleanup

--- pureMCMC/MCMC-tmp.py
+++ pureMCMC/MCMC-tmp.py
@@ -120,11 +120,11 @@ def calc_partition_distance(partition1, partition2):
 def main():
     random.seed(1)
     np.random.seed(1)
-    no_nodes = 10
+    no_nodes = 6
     no_colors = 3
     edge_probability = 0.3
     sample_size = 1000
-    MCMC_iterations = 25_000
+    MCMC_iterations = 100_000
     start_with_GES_DAG = True
     no_chains = 3
 

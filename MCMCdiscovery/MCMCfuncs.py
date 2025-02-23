fixed precision error with weights

MCMCdiscovery/MCMCfuncs.py
@@ -16,7 +16,7 @@ def CausalMCMC(samples, num_iters, mode = "bic", start_from_GES = False, move_we
     # Check that wieghts are legal
     if move_weights is not None:
         p_change_color, p_add, p_remove = move_weights
-        if p_change_color<0 or p_add<0 or p_remove<0 or sum(move_weights)>1:
+        if p_change_color<0 or p_add<0 or p_remove<0 or not np.isclose(sum(move_weights),1):
             raise ValueError("invalid move probabilities")
     else:
         move_weights = [1/3]*3
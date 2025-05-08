fd05cef7c48c1eef2a341669c91c5ea7105b0660
Ismael Parada <ismaelp@kth.se>
2025-05-08 11:33:55 +0200

    cleanup

--- ColoredNodes/MCMCmap-tests/map-visual25.py
+++ ColoredNodes/MCMCmap-tests/map-visual25.py
@@ -10,7 +10,7 @@ sys.path.append("../")
 from collections import defaultdict
 
 import utils
-from MCMCfuncs import MCMC_iteration, score_DAG
+from MCMCfuncs import CausalMCMC, MCMC_iteration, score_DAG_full
 from utils import get_sorted_edges
 
 
@@ -78,10 +78,10 @@ def main():
     G = nx.Graph(edge_array)
     pos = nx.spectral_layout(G)
     
-    allbics = [score_DAG(samples, np.reshape(np.frombuffer(x, dtype="int"), (3,3)), real_partition)[0] for x in dags]
+    allbics = [utils.score_DAG(samples, np.reshape(np.frombuffer(x, dtype="int"), (3,3)), real_partition) for x in dags]
     node_size = [300*x for x in allbics]
     print(sorted(allbics))
-    print("True was", sorted(allbics).index(score_DAG(samples, real_A, real_partition)[0]), "best of 25")
+    print("True was", sorted(allbics).index(utils.score_DAG(samples, real_A, real_partition)), "best of 25")
 
 
     # setup for MCMC
@@ -92,10 +92,12 @@ def main():
         global current_edge_array
         global current_partition
         global current_bic
+        global current_ML_data
         global labels
         current_edge_array = np.zeros((3,3))
         current_partition = real_partition.copy()
-        current_bic = score_DAG(samples, current_edge_array, current_partition)
+        CausalMCMC(samples, 0)
+        current_bic, current_ML_data = score_DAG_full(current_edge_array, current_partition)
 
         labels = {i:0 for i in range(25)}
         labels[0] = 1
@@ -110,10 +112,12 @@ def main():
         global current_edge_array
         global current_partition
         global current_bic
+        global current_ML_data
         global current_node
 
         ax.clear()
-        current_edge_array, current_partition, current_bic, _ = MCMC_iteration(samples, current_edge_array, current_partition, current_bic, [0.4,0.6])
+        move = random.choices([0, 1], k=1, weights=[0.4,0.6])[0]
+        current_edge_array, current_partition, current_bic, current_ML_data, _ = MCMC_iteration(move, current_edge_array, current_partition, current_bic, current_ML_data)
 
         A = current_edge_array.astype("int")
         labels[dags.index(A.tobytes())] += 1

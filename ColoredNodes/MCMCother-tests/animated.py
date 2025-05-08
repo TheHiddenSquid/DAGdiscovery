4ebe774dfdf660e3b151e76efe4c334ea1c0bb13
Ismael Parada <ismaelp@kth.se>
2025-05-08 11:52:35 +0200

    cleanup

--- ColoredNodes/MCMCother-tests/animated.py
+++ ColoredNodes/MCMCother-tests/animated.py
@@ -1,6 +1,6 @@
+import random
 import sys
 
-import ges
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -16,7 +16,6 @@ def main():
     no_colors = 4
     edge_prob = 0.6
     sample_size = 1000
-    start_with_GES_DAG = False
 
     real_partition, real_lambda_matrix, real_omega_matrix = utils.generate_colored_DAG(no_nodes, no_colors, edge_prob)
     real_edge_array = np.array(real_lambda_matrix != 0, dtype="int")
@@ -26,7 +25,7 @@ def main():
 
 
     # Create plots
-    fig, ((ax11, ax12), (ax21, ax22), (ax31, ax32), (ax41, ax42)) = plt.subplots(4, 2)
+    fig, ((ax11, ax12), (ax21, ax22)) = plt.subplots(2, 2)
     plt.tight_layout()
 
 
@@ -38,48 +37,9 @@ def main():
 
 
 
-     # MCMC setup
-    if start_with_GES_DAG:
-        # GES estimate of graph
-        res = ges.fit_bic(data=samples)
-        GES_edge_array = res[0]
-        initial_edge_array = GES_edge_array.copy()
-
-       
-        # Take an initial DAG from the given GES CPDAG
-        double = []
-        for i in range(no_nodes):
-            for j in range(i+1, no_nodes):
-                if initial_edge_array[i,j] == 1 and initial_edge_array[j,i] == 1:
-                    double.append((i,j))
-                    initial_edge_array[i,j] = 0
-                    initial_edge_array[j,i] = 0
-
-        for edge in double:
-            new_edges = initial_edge_array.copy()
-            new_edges[edge[0], edge[1]] = 1
-            G = nx.DiGraph(new_edges)
-            if nx.is_directed_acyclic_graph(G):
-                initial_edge_array = new_edges
-                continue
-
-            new_edges = initial_edge_array.copy()
-            new_edges[edge[1], edge[0]] = 1
-            G = nx.DiGraph(new_edges)
-            if nx.is_directed_acyclic_graph(G):
-                initial_edge_array = new_edges
-                continue
-
-            raise ValueError("Could not create DAG")
-
-
-        # Initial coloring guess
-        initial_partition = utils.generate_random_partition(no_nodes, no_nodes)
-
-    else:
-        # Start with random DAG
-        initial_partition, initial_lambda_matrix, real_omega_matrix = utils.generate_colored_DAG(no_nodes, no_nodes, 0.5)
-        initial_edge_array = np.array(initial_lambda_matrix != 0, dtype="int")
+    # Start with random DAG
+    initial_partition, initial_lambda_matrix, real_omega_matrix = utils.generate_colored_DAG(no_nodes, no_nodes, 0.5)
+    initial_edge_array = np.array(initial_lambda_matrix != 0, dtype="int")
     
 
 
@@ -91,8 +51,8 @@ def main():
 
     current_edge_array = initial_edge_array.copy()
     current_partition = initial_partition.copy()
-    current_bic, current_ML_data = MCMCfuncs.score_DAG_full(samples, current_edge_array, current_partition)
     MCMCfuncs.CausalMCMC(samples, 0)
+    current_bic, current_ML_data = MCMCfuncs.score_DAG_full(current_edge_array, current_partition)
 
     plt.axes(ax12)
     G = nx.DiGraph(current_edge_array)
@@ -103,7 +63,7 @@ def main():
     # Setop for BIC plots
     global bics
     global cumsum
-    start_bic = MCMCfuncs.score_DAG_full(samples, current_edge_array, current_partition)[0]
+    start_bic = current_bic
     bics = [start_bic]
     cumsum = [start_bic]
 
@@ -124,7 +84,8 @@ def main():
         global current_ML_data
         global current_bic
 
-        current_edge_array, current_partition, current_bic, current_ML_data, _ = MCMCfuncs.MCMC_iteration(current_edge_array, current_partition, current_bic, current_ML_data, [0.4,0.6])
+        move = random.choices([0, 1], k=1, weights=[0.4,0.6])[0]
+        current_edge_array, current_partition, current_bic, current_ML_data, _ = MCMCfuncs.MCMC_iteration(move, current_edge_array, current_partition, current_bic, current_ML_data)
         
         G = nx.DiGraph(current_edge_array)
         nx.draw_circular(G, node_color=utils.generate_color_map(current_partition), with_labels=True)
@@ -154,47 +115,8 @@ def main():
         plt.title("Rolling Average")
 
 
-        # SHD for each iteration
-        plt.axes(ax31)
-        ax31.clear()
-        SHDs.append(utils.calc_SHD(real_edge_array, current_edge_array))
-
-        plt.plot(range(len(SHDs)), SHDs)
-        plt.xlabel("iterations")
-        plt.ylabel("SHD")
-        plt.title("SHD")
-
-        # Scatter for intuition
-        plt.axes(ax32)
-        ax32.clear()
-
-        plt.scatter(bics, SHDs)
-        plt.xlabel("BIC")
-        plt.ylabel("SHD")
-        plt.title("BIC and SHD relation")
-
-
-        # CHD for each iteration
-        plt.axes(ax41)
-        ax41.clear()
-        CHDs.append(utils.calc_CHD(real_partition, current_partition))
-
-        plt.plot(range(len(CHDs)), CHDs)
-        plt.xlabel("iterations")
-        plt.ylabel("CHD")
-        plt.title("CHD")
-
-        # Scatter for intuition
-        plt.axes(ax42)
-        ax42.clear()
-
-        plt.scatter(bics, CHDs)
-        plt.xlabel("BIC")
-        plt.ylabel("CHD")
-        plt.title("BIC and CHD relation")
-
-
-    ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=10)
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=10)
+    #ani.save(filename="MCMC_anim.gif", writer="pillow")
     plt.show()
 
 
